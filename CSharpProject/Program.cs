7110be173c98b7ea0eb381478b5c8369497e0c78
Soma Mahendran <Soma.Mahendran@gds.ey.com>
2021-09-13 13:02:49 +0530

    first C# project commit

---
+++ CSharpProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CSharpProject
+{ 
+    class Assignment4_StringArray
+    {
+        static void Main(string[] args)
+        {
+
+            String[] word1 = new string[2];
+            String[] word2 = new string[2];
+
+            String concatWord1 = "", concatWord2 = "";
+
+            Console.WriteLine("Enter the first string array values");
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                word1[i] = Console.ReadLine();
+
+
+            }
+
+            foreach (String word in word1)
+            {
+
+                concatWord1 += word;
+            }
+
+            Console.WriteLine("Enter the second string array values");
+
+            for (int i = 0; i < word2.Length; i++)
+            {
+                word2[i] = Console.ReadLine();
+
+            }
+
+            foreach (String word in word2)
+            {
+
+                concatWord2 += word;
+            }
+
+
+            if (concatWord1.Equals(concatWord2))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("'False");
+
+            }
+
+
+
+        }
+    }
+}
